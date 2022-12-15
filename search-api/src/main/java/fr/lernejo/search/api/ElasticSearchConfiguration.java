c2b6f644bdd56f8cfaf8289137d0947c11708362
Mdetorquat <maxence.de-torquat-de-la-coulerie@epita.fr>
2022-12-15 21:54:29 +0100

    partie 2

--- search-api/src/main/java/fr/lernejo/search/api/ElasticSearchConfiguration.java
+++ search-api/src/main/java/fr/lernejo/search/api/ElasticSearchConfiguration.java
@@ -1,5 +1,12 @@
 package fr.lernejo.search.api;
 
+import org.apache.http.HttpHost;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.CredentialsProvider;
+import org.apache.http.impl.client.BasicCredentialsProvider;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestClientBuilder;
 import org.elasticsearch.client.RestHighLevelClient;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -13,7 +20,12 @@ public class ElasticSearchConfiguration {
                                                    @Value("${elasticsearch.port:9200}") int port,
                                                    @Value("${elasticsearch.username:elastic}") String username,
                                                    @Value("${elasticsearch.password:admin}") String password) {
-        // TODO
-        return null;
+        final CredentialsProvider credentialsProvider = new BasicCredentialsProvider();
+        credentialsProvider.setCredentials(AuthScope.ANY, new UsernamePasswordCredentials(username, password));
+
+        RestClientBuilder restClientBuilder = RestClient.builder(new HttpHost(host, port)).setHttpClientConfigCallback(
+            httpClientBuilder -> httpClientBuilder.setDefaultCredentialsProvider(credentialsProvider));
+
+        return new RestHighLevelClient(restClientBuilder);
     }
 }
